9deb099adb82722494e2e0611ec827e3796a8193
Zainab Rasool <jainab.rasool@gmail.com>
2023-02-12 22:36:48 +0530

    updated servcie class

--- MyEmployee/src/main/java/com/company/myEmp/service/MyEmpService.java
+++ MyEmployee/src/main/java/com/company/myEmp/service/MyEmpService.java
@@ -1,7 +1,13 @@
 package com.company.myEmp.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.company.myEmp.Entity.MyEmpEntity;
 import com.company.myEmp.dto.MyEmpRequest;
@@ -23,8 +29,6 @@ public class MyEmpService {
 	@Autowired
 	MyEmpolyeeRepo empRepo;
 
-	
-
 	public MyEmpEntity ReqDtoToEntity(@RequestBody MyEmpRequest empReq) {
 
 		empE.seteId(empReq.geteId());
@@ -37,60 +41,66 @@ public class MyEmpService {
 	}
 
 	/*
-	 public MyEmpRequest EntiyToReqDto(@RequestBody MyEmpEntity empE) {
-
-		empReq.seteId(empE.geteId());
-		empReq.seteName(empE.geteName());
-		empReq.setePass(empE.getePass());
-		empReq.setAge(empE.getAge());
-
-		return empReq;
-
-	}public MyEmpResponse EntiyToResDto(@RequestBody MyEmpEntity empE) {
-
-		empRes.seteId(empE.geteId());
-		empRes.seteName(empE.geteName());
-		empRes.setePass(empE.getePass());
-		empRes.setAge(empE.getAge());
-
-		return empRes;
-
-	}
-
-	public MyEmpEntity ResDtoToEntity(@RequestBody MyEmpResponse empRes) {
-
-		empE.seteId(empRes.geteId());
-		empE.seteName(empRes.geteName());
-		empE.setePass(empRes.getePass());
-		empE.setAge(empRes.getAge());
-
-		return empE;
-
-	}
-*/
-	public MyEmpEntity updateEmployee(int eId, String eNewName) {
+	 * public MyEmpRequest EntiyToReqDto(@RequestBody MyEmpEntity empE) {
+	 * 
+	 * empReq.seteId(empE.geteId()); empReq.seteName(empE.geteName());
+	 * empReq.setePass(empE.getePass()); empReq.setAge(empE.getAge());
+	 * 
+	 * return empReq;
+	 * 
+	 * }public MyEmpResponse EntiyToResDto(@RequestBody MyEmpEntity empE) {
+	 * 
+	 * empRes.seteId(empE.geteId()); empRes.seteName(empE.geteName());
+	 * empRes.setePass(empE.getePass()); empRes.setAge(empE.getAge());
+	 * 
+	 * return empRes;
+	 * 
+	 * }
+	 * 
+	 * public MyEmpEntity ResDtoToEntity(@RequestBody MyEmpResponse empRes) {
+	 * 
+	 * empE.seteId(empRes.geteId()); empE.seteName(empRes.geteName());
+	 * empE.setePass(empRes.getePass()); empE.setAge(empRes.getAge());
+	 * 
+	 * return empE;
+	 * 
+	 * }
+	 */
+	public String updateEmployee(int eId, String eNewName) {
 		if (empRepo.existsById(eId)) {
 			empE.seteId(eId);
 			empE.seteName(eNewName);
-			System.out.println("updateEmployee-" + empE);
-			return empRepo.save(empE);
+			 empRepo.save(empE);
+			 return "Deatils are updated for Employee Id-"+eId;
 		}
 
 		else {
-			return null;
-			}
+			return "Enter a valid Employee Id";
+		}
 
 	}
-	
-	
+
 	public String deleteEmployee(int eId) {
 		if (empRepo.existsById(eId)) {
 			empRepo.deleteById(eId);
-			return "Deleted Student with id: " + eId;
+			return "Deleted Employee with id: " + eId;
 		} else
-			return "Enter a valid student Id!";
+			return "Enter a valid Employee Id!";
 
 	}
-	
+
+	public List<MyEmpEntity> gettAllEmpDetails() {
+		return empRepo.findAll();
+	}
+
+	public Optional<MyEmpEntity> getEmpById(@PathVariable @Valid int eId) {
+		return empRepo.findById(eId);
+
+	}
+
+	public String setEmployee(@RequestBody MyEmpRequest empReq) {
+		empRepo.save(ReqDtoToEntity(empReq));
+		return empReq.toString();
+	}
 
 }
